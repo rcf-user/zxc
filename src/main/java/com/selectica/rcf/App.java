6c9518bafaf6a32b27f090107687f0fe24f84ab4
Albert Battalov <abattalov@selectica.com>
2015-03-19 17:58:15 +0200

    zxc Thu Mar 19 17:58:15 EET 2015

---
+++ src/main/java/com/selectica/rcf/App.java
@@ -0,0 +1,12 @@
+package com.selectica.rcf;
+
+/**
+ * Created by abattalov on 08.10.2014.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
